27c356e86e17d7890075542b977f5a9b3e2c85f2
lucasliborio <lucasl.troncoso@gmail.com>
2022-07-15 14:39:25 -0300

    refactor: add new validations on update-post controller

--- src/main/controllers/post/update-post-controller.ts
+++ src/main/controllers/post/update-post-controller.ts
@@ -1,17 +1,25 @@
 import postModel from "../../db/mongo/models/post-model";
 import { Controller } from "../../protocols/api/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http/http-types";
-import { serverError } from "../../protocols/http/http-response";
+import { serverError, ok, badRequest } from "../../protocols/http/http-response";
 
 export class UpdatePostController implements Controller {
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const { postId, title, description } = request.body
+    const { title, description } = request.body
+    const { postId } = request.params
     try {
-      await postModel.findOneAndUpdate({ _id: postId },{
-        title,
-        description
-      })
+
+      const postUpdated = await postModel.findOneAndUpdate({_id: postId},{
+        title: title,
+        description: description
+
+      }, {runValidators: true, new:true})
+      console.log(postUpdated)
+      if (postUpdated) return ok(postUpdated)
+
+      return badRequest('invalid postId')
     } catch (err: any) {
+      if (err.name === "ValidationError") return badRequest('invalid body request')
       return serverError()
     }
   }
