642d05f73527412daf6a47194fa4c9a57d6f7b04
lucasliborio <lucasl.troncoso@gmail.com>
2022-07-15 14:38:01 -0300

    refactor: add new http response to delete-post-controller

--- src/main/controllers/post/delete-post-controller.ts
+++ src/main/controllers/post/delete-post-controller.ts
@@ -1,23 +1,23 @@
 import postModel from "../../db/mongo/models/post-model";
 import { Controller } from "../../protocols/api/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http/http-types";
-import { serverError, ok, unauthorized } from "../../protocols/http/http-response";
+import { serverError, ok, unauthorized, badRequest } from "../../protocols/http/http-response";
 
 export class DeletePostController implements Controller {
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const { postId, payload } = request.body
-
+    const { payload } = request.body
+    const { postId } = request.params
     try {
-      const postToDelete = await postModel.findById({
-        _id: postId
-      })
+      const postToDelete = await postModel.findById(postId)
 
-      if (postToDelete.profileId === payload.profileId) return ok(postModel.findByIdAndDelete(postId))
+      if(!postToDelete) return badRequest('not found')
+      
+      if (postToDelete.profileId.toString() === payload.profileId) return ok(await postModel.findByIdAndDelete(postId))
       else {
         return unauthorized()
       }
-
     } catch (err: any) {
+      if (err.name == "TypeError") return badRequest('invalid parameters')
       return serverError()
     }
   }
