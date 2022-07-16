c33de5128206f6d746824366a49aee8029bebf95
lucasliborio <lucasl.troncoso@gmail.com>
2022-07-16 11:14:51 -0300

    feat: ensure that profiles follow/unfollow each other

---
+++ src/main/controllers/profile/follow-unfollow-profile-controller.ts
@@ -0,0 +1,48 @@
+import profileModel from "../../db/mongo/models/profile-model";
+import { Controller } from "../../protocols/api/controller";
+import { badRequest, serverError, ok } from "../../protocols/http/http-response";
+import { HttpRequest, HttpResponse } from "../../protocols/http/http-types";
+
+export class FollowUnfollowProfileController implements Controller {
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    const { profileId } = request.params
+    const { payload } = request.body
+    const profileToFollowUnfollow = await profileModel.findById(profileId)
+    if (!profileToFollowUnfollow) return badRequest('profile dont exist')
+
+    const profileOwner = await profileModel.findById(payload.profileId)
+    try {
+      if (profileOwner) {
+        const indexToMatch = profileOwner.following.indexOf(profileToFollowUnfollow.id)
+        if (indexToMatch === -1) {
+
+          await profileModel.updateOne({ _id: payload.profileId }, {
+            $push: {
+              following: profileId
+            }
+          })
+          await profileModel.updateOne({ _id: profileId }, {
+            $push: {
+              followers: payload.profileId
+            }
+          })
+        } else {
+          await profileModel.updateOne({ _id: payload.profileId }, {
+            $pull: {
+              following: profileId
+            }
+          })
+          await profileModel.updateOne({ _id: profileId }, {
+            $pull: {
+              followers: payload.profileId
+            }
+          })
+        }
+        return indexToMatch === -1 ? ok({ sucess: 'follow profile successfully' }) : ok({ sucess: 'unfollow profile successfully' })
+      }
+    } catch (error) {
+      return serverError()
+    }
+
+  }
+}
