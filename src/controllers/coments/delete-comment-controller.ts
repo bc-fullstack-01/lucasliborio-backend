3ed7fcc2f0861d12e926b2812ab71ac95c39d7e9
lucasliborio <lucasl.troncoso@gmail.com>
2022-07-19 19:21:39 -0300

    refactor: refactor code errors

--- src/controllers/coments/delete-comment-controller.ts
+++ src/controllers/coments/delete-comment-controller.ts
@@ -2,29 +2,25 @@
 import commentModel from "../../db/mongo/models/comment-model";
 import postModel from "../../db/mongo/models/post-model";
 import { Controller } from "../../protocols/api/controller";
-import { serverError, ok, badRequest } from "../../protocols/http/http-response";
+import { ok, notFound, forbbiden } from "../../protocols/http/http-response";
 import { HttpRequest, HttpResponse } from "../../protocols/http/http-types";
 
 export class DeleteCommentController implements Controller {
   async handle(request: HttpRequest): Promise<HttpResponse> {
-
     const { commentId, postId } = request.params
-    try {
-      const postToDeleteComment = await postModel.findById(postId)
+    const { payload } = request.body
 
-      if (postToDeleteComment) {
-        const commentToDelete = await commentModel.findByIdAndDelete(commentId)
-        await postModel.findByIdAndUpdate(postId, {
-          $pull: {
-            comments: commentToDelete.id
-          }
-        })
+    const postToDeleteComment = await postModel.findById(postId)
+    if (postToDeleteComment.profileId.toString() === payload._id.toString()) return forbbiden()
+    if (!postToDeleteComment) return notFound('POST')
+
+    const commentToDelete = await commentModel.findByIdAndDelete(commentId)
+    if (!commentToDelete) return notFound('COMMENT')
+    await postModel.findByIdAndUpdate(postId, {
+      $pull: {
+        comments: commentToDelete.id
       }
-      return ok({ sucess: 'comment deleted successfully' })
-    } catch (error) {
-      console.log(error)
-      if (error.name == "ValidationError") return badRequest('invalid params')
-      return serverError()
-    }
+    })
+    return ok({ ok: 'comment deleted successfully' })
   }
 }
