89f9905ae961b436b0cded482ef254ef12505fec
lucasliborio <lucasl.troncoso@gmail.com>
2022-07-19 19:22:18 -0300

    refactor: dele code not necessary

--- src/controllers/coments/like-comment-controller.ts
+++ src/controllers/coments/like-comment-controller.ts
@@ -2,33 +2,29 @@ import { publishEvent } from "../../broker/pub";
 import commentModel from "../../db/mongo/models/comment-model";
 import postModel from "../../db/mongo/models/post-model";
 import { Controller } from "../../protocols/api/controller";
-import { ok, serverError } from "../../protocols/http/http-response";
+import { notFound, ok, serverError } from "../../protocols/http/http-response";
 import { HttpRequest, HttpResponse } from "../../protocols/http/http-types";
 
 export class LikeUnlikeCommentController implements Controller {
   async handle(request: HttpRequest): Promise<HttpResponse> {
     const { commentId } = request.params
-    const { _id } = request.body.payload
-    try {
-      
-      const commentToLike = await commentModel.findById(commentId)
-      const likeIndex = commentToLike.likes.indexOf(_id)
-      likeIndex === -1 
+    const { payload } = request.body
+
+    const commentToLike = await commentModel.findById(commentId)
+    if (commentToLike) return notFound("COMMENT")
+    
+    const likeIndex = commentToLike.likes.indexOf(payload._id)
+    likeIndex === -1
       ? await commentModel.findByIdAndUpdate(commentId, {
-        $push:{likes: _id}
+        $push: { likes: payload._id }
       }) : await commentModel.findByIdAndUpdate(commentId, {
-        $pull:{likes: _id}
+        $pull: { likes: payload._id }
       })
 
-      if (likeIndex === -1) {
-        await publishEvent('comment-like', commentToLike.profileId, commentToLike )
-        return ok({ sucess: 'comment liked successfully' })
-      }
-      else return ok({ sucess: 'comment unliked successfully' })
-
-    } catch (error) {
-      console.log(error)
-      return serverError()
+    if (likeIndex === -1) {
+      await publishEvent('comment-like', commentToLike.profileId, commentToLike)
+      return ok({ ok: 'comment liked successfully' })
     }
+    return ok({ ok: 'comment unliked successfully' })
   }
 }
