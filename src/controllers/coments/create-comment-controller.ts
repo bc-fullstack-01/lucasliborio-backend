a67f92ad8d5e03adb62eb1f6ac44916d0d1893ad
lucasliborio <lucasl.troncoso@gmail.com>
2022-07-19 19:21:25 -0300

    refactor: refactor code errors

--- src/controllers/coments/create-comment-controller.ts
+++ src/controllers/coments/create-comment-controller.ts
@@ -3,34 +3,28 @@ import { publishEvent } from "../../broker/pub";
 import commentModel from "../../db/mongo/models/comment-model";
 import postModel from "../../db/mongo/models/post-model";
 import { Controller } from "../../protocols/api/controller";
-import { serverError, ok, badRequest } from "../../protocols/http/http-response";
+import { serverError, ok, badRequest, notFound } from "../../protocols/http/http-response";
 import { HttpRequest, HttpResponse } from "../../protocols/http/http-types";
 
 export class CreateCommentController implements Controller {
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const { payload ,content } = request.body
+    const { payload, content } = request.body
     const { postId } = request.params
-    try {
-      const postToAddNewComment = await postModel.findById(postId)
 
-      if (postToAddNewComment) {
-        const createComment = await commentModel.create({
-          content,
-          profileId: payload._id,
-          postId
-        })
-        const postToUpdateComment = await postModel.updateOne({_id: postId},{$push: {
-          comments: createComment.id
-        }})
-        if (postToUpdateComment) {
-          await publishEvent('new-comment', postToAddNewComment.profileId, createComment)
-          return ok({sucess: 'comment added succesfully'})
-        }
+    const postToAddNewComment = await postModel.findById(postId)
+    if (!postToAddNewComment) return notFound('COMMENT')
+    const createComment = await commentModel.create({
+      content,
+      profileId: payload._id,
+      postId
+    })
+    await postModel.updateOne({ _id: postId }, {
+      $push: {
+        comments: createComment.id
       }
-      
-    } catch (error) {
-      if (error.name == "ValidationError") return badRequest('please try again')
-      return serverError()
-    }
+    })
+    await publishEvent('new-comment', postToAddNewComment.profileId, createComment)
+    return ok({ ok: 'comment added succesfully' })
   }
 }
+
